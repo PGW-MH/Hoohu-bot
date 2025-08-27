858d1f435f21d1f37863380ad3a844363ff325fb
Honoka55 <1915637207@qq.com>
2025-08-27 16:19:13 +0800

    fix: default module sandbox

--- scripts/sandbox-cleanup.js
+++ scripts/sandbox-cleanup.js
@@ -13,7 +13,7 @@ const PAGES = {
         summary: 'Sandbox cleanup. For long-term testing, please use [[Special:MyPage/sandbox|your personal sandbox]].'
     },
     'Module:Sandbox': {
-        content: 'local p = {}\n\n\nreturn p',
+        content: 'local p = {}\n\nfunction p.main(frame)\n    -- perform your test here\nend\n\nreturn p',
         summary: 'Sandbox cleanup. For long-term testing, please use “Module:Sandbox/Your_username” or its subpages.'
     }
 };
